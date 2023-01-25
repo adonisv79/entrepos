96d0a036c78178ca4eeb3110078187a2d4a494fb
Adonis Lee Villamor <adonisv79@gmail.com>
2023-01-25 20:42:17 +0800

    some header fixes

--- frontends/entrepos/src/pages/api/protected-api.ts
+++ frontends/entrepos/src/pages/api/protected-api.ts
@@ -4,13 +4,13 @@ import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0'
 // Protected API, requests to '/api/protected' without a valid session cookie will fail
 
 async function handle(req, res) {
-  const { user } = getSession(req, res)
+  const { user } = await getSession(req, res)
 
   try {
     res.status(200).json({
       session: 'true',
-      id: user.sub,
-      nickname: user.nickname,
+      id: user?.sub,
+      nickname: user?.nickname,
     })
   } catch (e) {
     res.status(500).json({ error: 'Unable to fetch', description: e })
--- frontends/entrepos/src/pages/index.tsx
+++ frontends/entrepos/src/pages/index.tsx
@@ -1,22 +1,27 @@
+import { useUser } from '@auth0/nextjs-auth0/client'
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
-export default () => (
-  <Layout title="EntrePOS - Home">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-      <button>CLICK</button>
-      <select>
-        <option>s</option>
-        <option>2</option>
-      </select>
-      <input type="text" />
-      <input type="color" />
-      <input type="checkbox" />
-      <input type="date" />
-      <input type="email" />
-      <input type="file" />
-    </p>
-  </Layout>
-)
+export default () => {
+  const { user, isLoading } = useUser()
+
+  return (
+    <Layout title="EntrePOS - Home" user={user} loading={isLoading}>
+      <h1>Hello Next.js 👋</h1>
+      <p>
+        <Link href="/about">About</Link>
+        <button>CLICK</button>
+        <select>
+          <option>s</option>
+          <option>2</option>
+        </select>
+        <input type="text" />
+        <input type="color" />
+        <input type="checkbox" />
+        <input type="date" />
+        <input type="email" />
+        <input type="file" />
+      </p>
+    </Layout>
+  )
+}
